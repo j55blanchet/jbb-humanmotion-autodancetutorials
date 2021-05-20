a22a5e87db50d5270dcd9fd5e0dc3aa347c5be71
Julien Blanchet <j55blanchet@gmail.com>
2021-05-20 13:05:12 -0400

    feat: reactive motion database

---
+++ src/components/elements/ActivityVideoPlayer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="activity-video-">
+    <PausingVideoPlayer />
+    <InstructionCarousel />
+    <WebcamBox />
+  </div>
+</template>
+
+<script lang="ts">
+
+import {
+  computed, ComputedRef, defineComponent, nextTick, onBeforeUnmount, onMounted, Ref, ref, toRefs, watch,
+} from 'vue';
+import InstructionCarousel, { Instruction } from '@/components/elements/InstructionCarousel.vue';
+import PausingVideoPlayer from '@/components/elements/PausingVideoPlayer.vue';
+import WebcamBox from '@/components/elements/WebcamBox.vue';
+
+const ActivityPlayState = Object.freeze({
+  AwaitingPlayGesture: 'AwaitingPlayGesture',
+  PendingStart: 'PendingStart',
+  Playing: 'Playing',
+  ActivityEnded: 'ActivityEnded',
+});
+
+export default defineComponent({
+  name: 'ActivityVideoPlayer',
+  props: {
+    dance: { type: Object },
+    lesson: { type: Object },
+    activity: { type: Object },
+    defaultPauseDuration: { type: Number, default: 1.5 },
+  },
+  components: {
+    PausingVideoPlayer,
+    InstructionCarousel,
+    WebcamBox,
+  },
+});
+
+</script>
+
+<style>
+
+</style>
