0742c1d07a7df507e8a015436601bbc5c208c3f9
Julien Blanchet <j55blanchet@gmail.com>
2021-06-09 10:04:13 -0400

    Adding activities list to video player

--- src/components/elements/VideoLessonPlayer.vue
+++ src/components/elements/VideoLessonPlayer.vue
@@ -1,65 +1,86 @@
 <template>
   <div class="video-lesson-player">
-    <ActivityVideoPlayer
-      class="block"
-      ref="activityVideoPlayer"
-      :motion="videoEntry"
-      :lesson="videoLesson"
-      :activity="activeActivity"
-      :maxHeight="maxVideoHeight"
-      @progress="onProgress"
-    />
+    <div class="columns">
+      <div class="column">
+        <ActivityVideoPlayer
+          class="block"
+          ref="activityVideoPlayer"
+          :motion="videoEntry"
+          :lesson="videoLesson"
+          :activity="activeActivity"
+          :maxHeight="maxVideoHeight"
+          @progress="onProgress"
+        />
+      </div>
+      <div class="column is-narrow">
+        <nav class="panel" :style="{
+              'max-height':maxVideoHeight,
+              'overflow-y': 'scroll',
+            }"
+            v-if="videoLesson">
+          <div class="panel-heading">Activities</div>
+          <a class="panel-block is-size-7"
+            v-for="(activity, i) in videoLesson.activities" :key="i"
+            :class="{'is-active': activeActivityIndex === i}"
+            @click="gotoActivity(i)"
+            >
+            <strong class="panel-icon">{{i+1}}&nbsp;</strong>&nbsp;{{activity.title}}
+          </a>
+        </nav>
+      </div>
+    </div>
+    <div>
+      <SegmentedProgressBar
+        class="block"
+        :segments="progressSegments"
+        :progress="activityProgress"
+      />
 
-    <SegmentedProgressBar
-      class="block"
-      :segments="progressSegments"
-      :progress="activityProgress"
-    />
-
-    <div class="buttons is-centered has-addons">
-      <button
-        class="button"
-        :disabled="!hasPreviousActivity"
-        @click="activeActivityIndex -= 1"
-      >
-        &lt;
-      </button>
-      <button
-        v-show="!needsStartWebcam"
-        class="button"
-        :class="{
-          'is-primary': ($refs.activityVideoPlayer?.awaitingStart ?? false)
-        }"
-        :disabled="!($refs.activityVideoPlayer?.awaitingStart ?? false)"
-        @click="$refs.activityVideoPlayer.play()"
-      >
-        Play
-      </button>
-      <button
-        class="button is-primary"
-        v-show="needsStartWebcam"
-        :class="{'is-loading': webcamStatus==='loading'}"
-        @click="startWebcam">
-        Start Webcam
-      </button>
-      <button
-        class="button"
-        :disabled="!($refs.activityVideoPlayer?.activityFinished ?? false)"
-        @click="$refs.activityVideoPlayer?.reset()"
-      >
-        Reset
-      </button>
-      <button
-        class="button"
-        :disabled="!hasNextActivity && !enableCompleteLesson"
-        :class="{
-          'is-primary': (!hasNextActivity && enableCompleteLesson) || ($refs.activityVideoPlayer?.activityFinished ?? false)
-        }"
-        @click="nextActivity"
-      >
-        <span v-if="hasNextActivity || !enableCompleteLesson">&gt;</span>
-        <span v-else>Complete Lesson</span>
-      </button>
+      <div class="buttons is-centered has-addons">
+        <button
+          class="button"
+          :disabled="!hasPreviousActivity"
+          @click="activeActivityIndex -= 1"
+        >
+          &lt;
+        </button>
+        <button
+          v-show="!needsStartWebcam"
+          class="button"
+          :class="{
+            'is-primary': ($refs.activityVideoPlayer?.awaitingStart ?? false)
+          }"
+          :disabled="!($refs.activityVideoPlayer?.awaitingStart ?? false)"
+          @click="$refs.activityVideoPlayer.play()"
+        >
+          Play
+        </button>
+        <button
+          class="button is-primary"
+          v-show="needsStartWebcam"
+          :class="{'is-loading': webcamStatus==='loading'}"
+          @click="startWebcam">
+          Start Webcam
+        </button>
+        <button
+          class="button"
+          :disabled="!($refs.activityVideoPlayer?.activityFinished ?? false)"
+          @click="$refs.activityVideoPlayer?.reset()"
+        >
+          Reset
+        </button>
+        <button
+          class="button"
+          :disabled="!hasNextActivity && !enableCompleteLesson"
+          :class="{
+            'is-primary': (!hasNextActivity && enableCompleteLesson && $refs.activityVideoPlayer?.activityFinished) || ($refs.activityVideoPlayer?.activityFinished ?? false)
+          }"
+          @click="nextActivity"
+        >
+          <span v-if="hasNextActivity || !enableCompleteLesson">&gt;</span>
+          <span v-else>Complete Lesson</span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -122,6 +143,9 @@ export default defineComponent({
     nextActivity() {
       if (this.hasNextActivity) this.activeActivityIndex += 1;
       else this.$emit('lesson-completed');
+    },
+    gotoActivity(i: number) {
+      this.activeActivityIndex = i;
     },
     async startWebcam() {
       return (this.$refs.activityVideoPlayer as any)?.startWebcam();
