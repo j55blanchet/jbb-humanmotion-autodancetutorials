f136e238c69fa7351c593b3ac3bf1ba7e0806a8d
Julien Blanchet <j55blanchet@gmail.com>
2022-04-14 09:46:12 -0400

    Updating Azure SAS Token

--- web-frontend/src/services/AzureUploader.ts
+++ web-frontend/src/services/AzureUploader.ts
@@ -4,7 +4,8 @@ import { BlobServiceClient } from '@azure/storage-blob';
 // const credential = new DefaultAzureCredential();
 
 const blobServiceClient = new BlobServiceClient(
-  'https://dartrobotlabstorage.blob.core.windows.net/study-video-uploads?sp=acw&st=2021-04-07T02:19:35Z&se=2022-04-07T10:19:35Z&sv=2020-02-10&sr=c&sig=9fpKagbOjUqAjHF579%2FW7KiM4U8YcBrbI8J70tH35Y4%3D',
+  'https://dartrobotlabstorage.blob.core.windows.net/study-video-uploads?sv=2020-08-04&ss=bf&srt=co&sp=wdacitfx&se=2025-04-14T21:31:43Z&st=2022-04-14T13:31:43Z&spr=https&sig=RoRBLMKyg%2FUB48b2M1MLDLc%2FIsdNhkK66KaHvL%2BI0Y0%3D',
+  // 'https://dartrobotlabstorage.blob.core.windows.net/study-video-uploads?sp=acw&st=2021-04-07T02:19:35Z&se=2022-04-07T10:19:35Z&sv=2020-02-10&sr=c&sig=9fpKagbOjUqAjHF579%2FW7KiM4U8YcBrbI8J70tH35Y4%3D',
   // credential,
 );
 
